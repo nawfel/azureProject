c40f6b5b4f4dc32cbf6c3202d10fa73814b8ff2c
nawfel <sghaier.nawfel@hotmail.com>
2023-11-08 22:22:48 +0100

    maj article model

--- azureProject/Models/Article.cs
+++ azureProject/Models/Article.cs
@@ -2,6 +2,24 @@
 {
     public class Article
     {
-       
+        public Article()
+        {
+            
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Content { get; set; }
+        public DateTime DateCreation { get; set; }
+        public DateTime DateModification { get; set; }
+        public User User { get; set; }
+        public int UserId { get; set; }
+        public Comment Comment { get; set; }
+        public int CommentId { get; set; }
+
+        public string ImageUrl { get; set; }
+
+
     }
 }
