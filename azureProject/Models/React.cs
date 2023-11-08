89af8b9ca525591d7040e641cc17086e6333d110
nawfel <sghaier.nawfel@hotmail.com>
2023-11-08 22:41:26 +0100

    update model

--- azureProject/Models/React.cs
+++ azureProject/Models/React.cs
@@ -2,5 +2,12 @@
 {
     public class React
     {
+        public int Id { get; set; }
+        public int LikesCounter { get; set; }
+
+        public int InsightfulCounter { get; set; }
+
+        public int DidntKnowThatCount { get; set; }
+
     }
 }
